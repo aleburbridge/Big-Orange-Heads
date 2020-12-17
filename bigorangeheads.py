9f4b85f633657fedca9d9364e8b15db68549e805
Alexander Bridgeman <aleburbridge@gmail.com>
2020-12-17 07:22:41 -0500

    v3

--- bigorangeheads.py
+++ bigorangeheads.py
@@ -1,41 +1,51 @@
-# Text-based party game! 
+ # Text-based party game! 
 # View the rules on the github page: ######
 
 import os
 import random
 import time
 
+def gain_250g():
+    gold = humanplayer['gold']
+    gold += 250
+    humanplayer['gold'] = gold
+gain250 = gain_250g
+
+wishDict = {
+    # gold wishes
+    "Gain 250g": gain250,
+    "Gain +100g for 3 turns (300g)": gain250,
+    "Gain +75g for 5 turns (375g)": gain250,
+    "Gold over time effects are worth +50%": gain250,
+    "Gain 250g for each human with a big orange head": gain250,
+    "Pick a human to receive 2x gold next gold wish (you can pick yourself)": gain250,
+
+    # wishes about wishes
+    "Gain two extra wishes": gain250,
+    "Gain an extra wish option next turn": gain250,
+    "Guarantee a gold wish option for the next three turns": gain250,
+
+    # dice rolls
+    "Roll a d12, gain 10x the gold shown": gain250,
+    "Roll a d12. If over 5, +400g. If 5 or under, +100g over 4 turns": gain250,
+
+    # wishes that improve odds
+    "Human dice rolls have a 50\% chance to show a 12 (instead of 8%)": gain250,
+    "Increase chance for legendary wishes to spawn (10% -> 20%)": gain250, # replace these with variables
+
+    # items
+    "Genie's lamp (Genie goes back in lamp and humans pick twist)": gain250,
+    "Magic piggie bank (50g per turn for the rest of the game)": gain250,
+    "Magic Wand (pick a big orange head to remove for 3 turns)": gain250
+}
+
 clear_screen_command = "cls" if input("Are you on Windows or Mac?\n" +
                                       "Enter W for Windows or anything else for Mac\n> ").strip().lower() == "w" else "clear"
 os.system(clear_screen_command)
 
-wishList = [
-    # gold wishes
-    "Gain 250g",
-    "Gain +100g for 3 turns (300g)",
-    "Gain +75g for 5 turns (375g)",
-    "Gold over time effects are worth +50%",
-    "Gain 250g for each human with a big orange head",
-    "Pick a human to receive 2x gold next gold wish (you can pick yourself)",
-
-    # wishes about wishes
-    "Gain two extra wishes",
-    "Gain an extra wish option next turn",
-    "Guarantee a gold wish option for the next three turns",
-
-    # dice rolls
-    "Roll a d12, gain 10x the gold shown",
-    "Roll a d12. If over 5, +400g. If 5 or under, +100g over 4 turns",
-
-    # wishes that improve odds
-    "Human dice rolls have a 50\% chance to show a 12 (instead of 8%)",
-    "Increase chance for legendary wishes to spawn (10% -> 20%)", # replace these with variables
-
-    # items
-    "Genie's lamp (Genie goes back in lamp and humans pick twist)",
-    "Magic piggie bank (50g per turn for the rest of the game)",
-    "Magic Wand (pick a big orange head to remove for 3 turns)"
-]
+wishList = []
+for key in wishDict.keys(): 
+    wishList.append(key)
 
 twistList = [
     # gold twists
@@ -70,8 +80,10 @@ twistList = [
 line_break = "--------------------------------------------------------------------"
 
 def displayIntro():
+    
     print('~~~BIG ORANGE HEADS~~~')
     input('press enter to begin :)')
+
 
 def get_list_players():
     return [x.lower() for x in input("Enter the names of the players separated by a space \n").split()]
@@ -88,23 +100,26 @@ def create_suspense():
     time.sleep(2)
 
 
-displayIntro()
-print()
-
 list_players = get_list_players()
 number_of_players = len(list_players)
 genie_player = list_players[(random.randint(0, (int(number_of_players) - 1)))]
 dictionary_list_players = []
 
-for item in list_players:
-    item = { 
-        'name': item, 
+playerNumber = 1
+
+for humanplayer in list_players:
+    humanplayer = { 
+        'name': humanplayer, 
         'gold': 0, 
         'big_orange_head': 'no', 
         'magic wand': 'no', 
-        'wishes': 3 
+        'wishes': 3,
+        'player_number': playerNumber
         }
-    dictionary_list_players.append(item)
+    playerNumber += 1
+    dictionary_list_players.append(humanplayer)
+
+
 
 # displays info at the top of the screen!
 def gold_tracker():
@@ -115,47 +130,68 @@ def gold_tracker():
                 gold += int(dictionary_list_players[index][key])
     return gold
 
-
-
 humanItems = []
 genieItems = []
 humanGoldTotal = gold_tracker()
-wishesLeft = ""
+wishesLeft = number_of_players * 3
 winningGoldAmount = number_of_players * 500
 
 
-def game_info():
+def game_info(turn):
     print("gold - " + str(humanGoldTotal))
     print("items - ")
+    print("Turn #" + str(turn))
+    print("Wishes left - " + str(wishesLeft))
+    player_number_check(turn)
 
-def display_wishes():
-    game_info()
+def player_number_check(turn):
+    for humanplayer in dictionary_list_players:
+        if humanplayer['player_number'] == turn:
+            print("Wisher: " + humanplayer['name'])
+
+
+def display_wishes(turn):
+    game_info(turn)
     create_suspense()
 
-    print("The wishes for this round are")
+
+    print("~~ WISHES ~~")
     firstWish = ("1: " + wishList[random.randint(0, (len(wishList) - 1 ))])
     secondWish = ("2: " + wishList[random.randint(0, (len(wishList) - 1 ))])
     print(firstWish)
     print(line_break)
-    print(twistList[random.randint(0, (len(twistList) - 1 ))])
-    print(twistList[random.randint(0, (len(twistList) - 1 ))])
-    print(twistList[random.randint(0, (len(twistList) - 1 ))])
+    print("a. " + twistList[random.randint(0, (len(twistList) - 1 ))])
+    print("b. " + twistList[random.randint(0, (len(twistList) - 1 ))])
+    print("c. " + twistList[random.randint(0, (len(twistList) - 1 ))])
     print()
     print(secondWish)
     print(line_break)
-    print(twistList[random.randint(0, (len(twistList) - 1 ))])
-    print(twistList[random.randint(0, (len(twistList) - 1 ))])
-    print(twistList[random.randint(0, (len(twistList) - 1 ))])
+    print("a. " + twistList[random.randint(0, (len(twistList) - 1 ))])
+    print("b. " + twistList[random.randint(0, (len(twistList) - 1 ))])
+    print("c. " + twistList[random.randint(0, (len(twistList) - 1 ))])
+    wisherResponse = input("\nWisher, type the number of your wish and press enter\n")
+    if wisherResponse == "1":
+        wishGranted = firstWish
+    elif wisherResponse == "2":
+        wishGranted = secondWish
 
+    turn += 1
 
-create_suspense()
+def startFirstRound():
+    input("Press enter to start the first round!")
+    os.system(clear_screen_command)
 
-print("The genie for this game is " + genie_player + "!!!")
+def startgame():
+    displayIntro()
+    print()
+    create_suspense()
+    print("The genie for this game is " + genie_player + "!!!")
+    startFirstRound()
 wishCounter = len(list_players)
 
-input("Press enter to start the first round!")
-os.system(clear_screen_command)
 
 
-display_wishes()
+startgame()
+display_wishes(1)
+
 
