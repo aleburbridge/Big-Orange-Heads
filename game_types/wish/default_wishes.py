942c15aae9a1d78fa0059c3c5132d0a4fb57ffdf
aleburbridge <aleburbridge@gmail.com>
2020-12-20 17:29:23 -0500

    Added big orange head wish ;)

--- game_types/wish/default_wishes.py
+++ game_types/wish/default_wishes.py
@@ -11,7 +11,7 @@ def default_wishes():
         #Wish("Gain 220 gold", Rarity.COMMON, actions.active_gain_gold(220), [Tag.GOLD, Tag.INSTANT_GOLD]),
         Wish("Big Orange Head", Rarity.COMMON, actions.big_orange_head_wish(), [Tag.BIG_O_HEAD]),
         Wish("Roll a d12. Gain 20x the gold shown", Rarity.COMMON, actions.d12_gold_roll(20), [Tag.GOLD, Tag.INSTANT_GOLD]),
-        Wish("Gain 250g for each human with a big orange head", Rarity.COMMON, actions.gain_gold_for_orange_heads(250), [Tag.GOLD, Tag.INSTANT_GOLD], Tag.BIG_O_HEAD),
+        Wish("Gain 250g for each human with a big orange head", Rarity.COMMON, actions.gain_gold_for_orange_heads(250), [Tag.GOLD, Tag.INSTANT_GOLD, Tag.BIG_O_HEAD]),
         
         Wish("Gain 500 gold", Rarity.LEGENDARY, actions.active_gain_gold(500), [Tag.GOLD, Tag.INSTANT_GOLD]),
         Wish("The next gold wish doubles the amount of gold you receive", Rarity.LEGENDARY, actions.multiply_next_gold_amount(2, 1), [Tag.GOLD])
