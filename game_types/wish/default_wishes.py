1d916a40ca8f8431e57862e0a877e87d04577b14
grant <mullinaxgr@gmail.com>
2020-12-20 17:25:15 -0500

    fixed alexs dummy changes

--- game_types/wish/default_wishes.py
+++ game_types/wish/default_wishes.py
@@ -9,7 +9,7 @@ def default_wishes():
         #Wish("Gain 250 gold", Rarity.COMMON, actions.active_gain_gold(250), [Tag.GOLD, Tag.INSTANT_GOLD]),
         #Wish("Gain 230 gold", Rarity.COMMON, actions.active_gain_gold(230), [Tag.GOLD, Tag.INSTANT_GOLD]),
         #Wish("Gain 220 gold", Rarity.COMMON, actions.active_gain_gold(220), [Tag.GOLD, Tag.INSTANT_GOLD]),
-        Wish("Big Orange Head", Rarity.COMMON, actions.big_orange_head_wish(), [Tag.BIG_O_HEAD])
+        Wish("Big Orange Head", Rarity.COMMON, actions.big_orange_head_wish(), [Tag.BIG_O_HEAD]),
         Wish("Roll a d12. Gain 20x the gold shown", Rarity.COMMON, actions.d12_gold_roll(20), [Tag.GOLD, Tag.INSTANT_GOLD]),
         Wish("Gain 250g for each human with a big orange head", Rarity.COMMON, actions.gain_gold_for_orange_heads(250), [Tag.GOLD, Tag.INSTANT_GOLD], Tag.BIG_O_HEAD),
         
