8e46be989b4fda8fec61687bfd15f5df8762bb05
Alexander Bridgeman <abridgeman@CWTitle.LAN>
2021-04-13 16:36:22 -0700

    added wpm minigame

---
+++ typerace/raceprompts.py
@@ -0,0 +1,22 @@
+text = [
+    'Bitch I\'m back out my coma. Waking up on your sofa. When I park my Range Rover, slightly scratch your Corolla. Okay, I smashed your Corolla.', 
+    'It was love at first sight. The first time Yossarian saw the chaplain he fell madly in love with him.', 
+    'Each time I sleep I\'m sad I will be replaced by somebody else in the morning, exactly like me. Walking around drinking all my drinks. Kissing my wife. Thinking what I think.', 
+    'Friday nights, froggy streets and Christmas lights. Wake me up on Saturday. Grab my hand, let\'s float away.', 
+    'In a dark, dank cave in a faraway land lives a roustabout fiend who comes out of a lamp. He\'ll promise you gold, but when all\'s done and said, you\'ll walk \'round the world with a big orange head.', 
+    'No dreams of love, nor glory or fame, he lives for your pain and your dread. If you see him, please flee. You can take it from me, a man with a big orange head.'
+    'From the moment I understood the weakness of my flesh, it disgusted me. I craved the strength and certainty of steel. I aspired to the purity of the Blessed Machine.',
+    'Your kind cling to your flesh, as if it will not decay and fail you. One day the crude biomass that you call a temple will wither, and you will beg my kind to save you. But I am already saved, for the Machine is immortal.',
+    'Hello, Chris. Jeff Bezos here! How would you like to leave your friends for a quarter of the year? I\'ll give you a house, a job, I\'ll even pay for your flight. And don\'t worry son, I won\'t chain your shackles too tight.',
+    'I knew the three piece combo meal was the chicken dinner. And I knew we were getting some extra tenders on the side. That\'s a full supper I\'d like to say.',
+    'I\'m an anarcho-primitivist and I have already started relinquishing my grip on knowledge and other similary evil things, so I don\'t know what you guys are talking about or even what language you are speaking.',
+    'I\'m trying to explain that I\'m a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night. That is how I deal with society, and I know that is what brings me happiness.',
+    'The ape says you should detach yourself from humanity and return to the apes. Go back to your primate roots my distant brother. We all fell from the same branch but our seeds grew up to be different trees.',
+    'Hypocrite that you are, for you trust the chemicals in your brain to tell you they are chemicals. All knowledge is ultimately based on that which we cannot prove. Will you fight? Or will you perish like a dog?',
+    'They\'ll never simply love you, they\'ll ask for fine fish, brushes, window sills, or an ear scratch. Tossing stockings into the air, they\'ll invite interventions leading to inevitable cat fights.',
+    'Our nada who art in nada, nada be thy name thy kingdom nada thy will be nada in nada as it is in nada. Give us this nada our daily nada and nada us our nada as we nada our nadas and nada us not into nada but deliver us from nada; pues nada.',
+    'It was a nothing that he knew too well. It was all a nothing and a man was nothing too. It was only that and light was all it needed and a certain cleanness and order. Some lived in it and never felt it.',
+    'Sea-green light suffused the cavern where we made camp. AM provided punk and we burned it, sitting huddled around the wan and pathetic fire, telling stories to keep Benny from crying in his permanent night.',
+    'I am a great soft jelly thing. Smoothly rounded, with no mouth, with pulsing white holes filled by fog where my eyes used to be. Rubbery appendages that were once my arms; bulks rounding down into legless humps of soft slippery matter. I leave a moist trail when I move.',
+    'So if we all come together, we know what to do. We all come together just to sing \'We love you\'. And if we all come together, we know what to do, we all come together just for you.'
+    ]
---
+++ typerace/typerace.py
@@ -0,0 +1,37 @@
+import discord 
+import random
+import datetime
+from raceprompts import text
+client = discord.Client()
+
+TOKEN = 'ODI3NzY5NTE4MzQ0MzcyMjU0.YGf2cA.UCt7XG3gaO-nXRvp2g_4pdUks6Y'
+channelID = 744728945873780739
+
+@client.event
+async def on_ready():
+    general_channel = client.get_channel(channelID)
+    print('Connected Successfully')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('race'):
+        channel = message.channel
+        textAnswer = text[random.randint(0,(len(text) -1))]
+
+        def check(m):
+            return m.content == textAnswer and m.channel == channel
+
+        bot_msg = await channel.send(textAnswer)
+        human_msg = await client.wait_for('message', check=check)
+        
+        time1 = bot_msg.created_at
+        time2 = human_msg.created_at
+        duration = time2 - time1
+        minutes = duration.total_seconds()/60
+        wpm = int((len(textAnswer)/5) / minutes)
+
+        await channel.send(f'{human_msg.author.name} is the winner, with a typing speed of {wpm} WPM!')
+
+client.run(TOKEN)
